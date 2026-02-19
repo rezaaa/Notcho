227b3545bf49b599a7d0d296267bfc89ea95755d
Reza <rezamahmudy1@gmail.com>
2026-02-19 21:01:02 +0330

    Use remote DynamicNotchKit dependency for CI builds

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
         .executable(name: "NotchTasks", targets: ["NotchTasks"])
     ],
     dependencies: [
-        .package(path: "LocalPackages/DynamicNotchKit"),
+        .package(url: "https://github.com/MrKai77/DynamicNotchKit.git", revision: "36e728b91fec43d3427a846497f5b1735e539153"),
         .package(url: "https://github.com/sparkle-project/Sparkle.git", from: "2.7.0")
     ],
     targets: [
         .executableTarget(
             name: "NotchTasks",
             dependencies: [
-                .product(name: "DynamicNotchKit", package: "DynamicNotchKit"),
+                .product(name: "DynamicNotchKit", package: "dynamicnotchkit"),
                 .product(name: "Sparkle", package: "Sparkle")
             ],
             path: "Sources/NotchTasks"
